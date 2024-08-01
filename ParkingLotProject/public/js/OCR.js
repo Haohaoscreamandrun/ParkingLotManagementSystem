251a1dcece72fe9abe396286fb437a68f590d702
“ChenHao” <j842671395@gmail.com>
2024-08-01 11:08:20 +0800

    Feats: html of OCR

---
+++ ParkingLotProject/public/js/OCR.js
@@ -0,0 +1,10 @@
+let fileSelector = document.querySelector('input')
+let start = document.querySelector('button')
+let img = document.querySelector('img')
+let progress = document.querySelector('.progress')
+let textarea = document.querySelector("textarea")
+
+//Show image uploaded
+fileSelector.onchange = () => {
+  
+}
