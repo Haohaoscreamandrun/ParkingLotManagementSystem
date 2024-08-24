21c0aaf4735395e4d921ee9e080e245282e8ea80
Jimmy Chen <j842671395@gmail.com>
2024-08-24 20:09:59 +0800

    FEATS: third post but need cardholder

--- ParkingLotProject/app/routers/third.py
+++ ParkingLotProject/app/routers/third.py
@@ -1,4 +1,5 @@
-from fastapi import *
+import requests
+from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
@@ -23,4 +24,25 @@ router = APIRouter(
 )
 async def payment_credit(postPayment: PostPrimePayment):
   print(postPayment.prime, postPayment.car.id, postPayment.car.sub_total)
+  # make request to Tappay
+  tappay_url = 'https://sandbox.tappaysdk.com/tpc/payment/pay-by-prime'
+  partner_key = 'partner_MWolLkvQ1R4JQGVp6W2N7yNl15PEYfNBSRUlV14n5TxAbr6oxz33YmSK'
+  merchant_id = 'J842671395_TAISHIN'
+  response_obj = requests.post(
+    url= tappay_url,
+    headers={
+      'Content-Type': 'application/json',
+      'x-api-key': partner_key
+    },
+    json={
+      'prime': postPayment.prime,
+      'partner_key': partner_key,
+      'merchant_id': merchant_id,
+      'details': f'Payment for carID:{postPayment.car.id}',
+      'amount': postPayment.car.sub_total
+    }
+  )
+  response = response_obj.json()
+  print(response)
+
   pass
